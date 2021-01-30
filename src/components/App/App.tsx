13c58c9940e7dc751dfd3f3653941db01de831f7
wravel <wravel@gmail.com>
2021-01-30 19:55:37 +0100

    Add Recorder start and stop action

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Recorder from '../Recorder';
 
 function App() {
   return (
     <div className="App">
-     Hello
+      <Recorder></Recorder>
     </div>
   );
 }
---
+++ src/redux/store.ts
@@ -0,0 +1,14 @@
+import { combineReducers, createStore } from "redux";
+import recorderReducer from "./recorder";
+import userEventsReducer from "./user-event";
+
+const rootReducer = combineReducers({
+  userEvents: userEventsReducer,
+  recorder: recorderReducer
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+const store = createStore(rootReducer);
+
+export default store;
